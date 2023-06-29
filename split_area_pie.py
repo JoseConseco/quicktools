a968696e14f31a9976218af9197cf7f67ec06f00
JoseConseco <bartosz.styperek@gmail.com>
2023-06-29 10:25:41 +0200

    fix calc of split zone

--- split_area_pie.py
+++ split_area_pie.py
@@ -28,49 +28,41 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
                                                     # ("FILE_BROWSER", "File Browser", ""),
                                                     # ("PREFERENCES", "Preferences", ""),
                                                     # ("EMPTY", "Empty", ""),
-                                                    ("UV", "UV", ""),
+                                                    ("UV_EDITOR", "UV", ""),
                                                     # ("SEQUENCE", "Sequence", ""),
                                                 ))
 
     def draw_callback_px(tmp, self, context):
         # draw horizontal line or vertical line through area, depending on self.mouse_x and self.mouse_y
-        print('drawing')
         area = context.area
 
         # get the area's width and height
         width = area.width
         height = area.height
 
-        # get the area's center
-        center_x = area.x + width / 2
-        center_y = area.y + height / 2
-
         # get the mouse position relative to the area's center
-        mouse_x = self.mouse_x - center_x
-        mouse_y = self.mouse_y - center_y
-
-        # get the mouse position as a vector
-        mouse_vector = Vector((mouse_x, mouse_y))
+        mouse_x = self.mouse_x - area.x
+        mouse_y = self.mouse_y - area.y
 
         # get the mouse position as a percentage of the area's width and height
-        mouse_x_percent = mouse_x / width
-        mouse_y_percent = mouse_y / height
+        mouse_x_percent = mouse_x / width - 0.5 # remap (0,1) to (-0.5, 0.5)
+        mouse_y_percent = mouse_y / height - 0.5 # remap (0,1) to (-0.5, 0.5)
 
         line_points = []
         rect_points = [] #
         if abs(mouse_x_percent) > abs(mouse_y_percent):
-            line_points = [(self.mouse_x, 0), (self.mouse_x, height)]
+            line_points = [(mouse_x, 0), (mouse_x, height)]
             if mouse_x_percent > 0: # right
-                rect_points = [(self.mouse_x, 0), (width, 0), (self.mouse_x, height), (width, height)]
+                rect_points = [(mouse_x, 0), (width, 0), (mouse_x, height), (width, height)]
             else: # left
-                rect_points = [(0, 0), (self.mouse_x, 0), (0, height), (self.mouse_x, height)]
+                rect_points = [(0, 0), (mouse_x, 0), (0, height), (mouse_x, height)]
 
         else:
-            line_points = [(0, self.mouse_y), (width, self.mouse_y)]
+            line_points = [(0, mouse_y), (width, mouse_y)]
             if mouse_y_percent > 0: # top
-                rect_points = [(0, self.mouse_y), (width, self.mouse_y), (0, height), (width, height)]
+                rect_points = [(0, mouse_y), (width, mouse_y), (0, height), (width, height)]
             else: # bottom
-                rect_points = [(0, 0), (width, 0), (0, self.mouse_y), (width, self.mouse_y)]
+                rect_points = [(0, 0), (width, 0), (0, mouse_y), (width, mouse_y)]
 
 
         batch = batch_for_shader(shader, 'TRIS', {"pos": rect_points}, indices=indices)
@@ -91,7 +83,8 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
         self.mouse_x = event.mouse_x
         self.mouse_y = event.mouse_y
         args = (self, context)
-        self._handle = bpy.types.SpaceView3D.draw_handler_add(
+        # self._handle = bpy.types.SpaceView3D.draw_handler_add(
+        self._handle = context.space_data.draw_handler_add(
             self.draw_callback_px, args, "WINDOW", "POST_PIXEL")
 
         context.window_manager.modal_handler_add(self)
@@ -121,21 +114,28 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
         width = area.width
         height = area.height
 
+        # print(f"{area.x=}")
+        # print(f"{self.mouse_x=}")
+
+
         # get the area's center
-        center_x = area.x + width / 2
-        center_y = area.y + height / 2
+        center_x = width / 2
+        center_y = height / 2
 
         # get the mouse position relative to the area's center
-        mouse_x = self.mouse_x - center_x
-        mouse_y = self.mouse_y - center_y
+        mouse_x = self.mouse_x - area.x
+        mouse_y = self.mouse_y - area.y
+
+        # print(f"{mouse_x=}")
 
         # get the mouse position as a vector
         mouse_vector = Vector((mouse_x, mouse_y))
 
         # get the mouse position as a percentage of the area's width and height
-        mouse_x_percent = mouse_x / width
-        mouse_y_percent = mouse_y / height
+        mouse_x_percent = mouse_x / width - 0.5 # remap (0,1) to (-0.5, 0.5)
+        mouse_y_percent = mouse_y / height - 0.5 # remap (0,1) to (-0.5, 0.5)
 
+        print(f"{mouse_x_percent=}")
 
         if abs(mouse_x_percent) > abs(mouse_y_percent):
             if mouse_x_percent > 0:
@@ -148,13 +148,13 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
             else:
                 split = 'BOTTOM'
 
-        print(f'mouse_x_percent: {mouse_x_percent}')
-        print(f'mouse_y_percent: {mouse_y_percent}')
-        print(f'split: {split}')
+        # print(f'mouse_x_percent: {mouse_x_percent}')
+        # print(f'mouse_y_percent: {mouse_y_percent}')
+        # print(f'split: {split}')
 
-        old_area = context.area
+        # old_area = context.area
         if split in ('RIGHT','LEFT'):
-            bpy.ops.screen.area_split( direction='VERTICAL', factor=self.mouse_x/width) # 'INVOKE_DEFAULT',
+            bpy.ops.screen.area_split( direction='VERTICAL', factor=mouse_x/width) # 'INVOKE_DEFAULT',
             # if split == 'RIGHT':
             #     old_area.type = self.new_area_type
             #     if self.new_area_type == 'NODE_EDITOR':
@@ -163,7 +163,7 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
 
 
         else:
-            bpy.ops.screen.area_split( direction='HORIZONTAL', factor=self.mouse_y/height) #'INVOKE_DEFAULT',
+            bpy.ops.screen.area_split( direction='HORIZONTAL', factor=mouse_y/height) #'INVOKE_DEFAULT',
 
         if self.new_area_type == 'NODE_EDITOR':
             context.screen.areas[-1].type = 'NODE_EDITOR'
@@ -171,14 +171,23 @@ class QUICKT_OT_SplitAreaPie(bpy.types.Operator):
         elif self.new_area_type == 'SHADER_EDITOR':
             context.screen.areas[-1].type = 'NODE_EDITOR'
             context.screen.areas[-1].ui_type = 'ShaderNodeTree'
+        elif self.new_area_type == 'IMAGE_EDITOR':
+            context.screen.areas[-1].type = 'IMAGE_EDITOR'
+            context.screen.areas[-1].ui_type = 'ShaderNodeTree'
+        elif self.new_area_type == 'UV_EDITOR':
+            context.screen.areas[-1].type = 'IMAGE_EDITOR'
+            context.screen.areas[-1].ui_type = 'UV'
         else:
             context.screen.areas[-1].type = self.new_area_type
 
-        bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
+        context.space_data.draw_handler_remove(self._handle, "WINDOW")
+        context.area.tag_redraw()
         return {"FINISHED"}
 
     def cancelled(self, context):
-        bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
+        # bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
+        context.space_data.draw_handler_remove(self._handle, "WINDOW")
+        context.area.tag_redraw()
         return {"CANCELLED"}
 
 
@@ -201,5 +210,6 @@ class QUICKT_MT_SplitAreaPie(bpy.types.Menu):
         pie.operator("screen.split_area_pie", text="Console").new_area_type = 'CONSOLE'
         pie.operator("screen.area_close", text="Close Area", icon='X')
         pie.operator("screen.split_area_pie", text="Shader Editor").new_area_type = 'SHADER_EDITOR'
+        pie.operator("screen.split_area_pie", text="UV Edit").new_area_type = 'UV_EDITOR'
 
 
